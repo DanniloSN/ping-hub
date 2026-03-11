ee6a6fd366c78043f0fb39bcd45cbffb38090d48
Danilo de Souza Nunes <dannilosn@gmail.com>
2026-03-11 10:01:50 -0300

    feat: add remove confirmation to CardItem

--- src/lib/components/CardItem.svelte
+++ src/lib/components/CardItem.svelte
@@ -21,20 +21,28 @@
 
 	const { instance, removeItem }: Props = $props();
 	const responseTimeInMs = instance.responseTimeMs;
+
+	function confirmRemove() {
+		if (confirm(`Tem certeza que deseja remover a instância "${instance.name}"?`)) {
+			removeItem(instance.url);
+		}
+	}
 </script>
 
 <Card>
 	<div class="flex items-center justify-between">
-		<Link href={instance.url} class="-scale-x-100 p-2" external>
-			<Icon icon="gridicons:external" />
-		</Link>
 		<Link class="flex items-center" href={`/edit/${instance.id}`}>
 			<img class="h-5 w-5 object-cover" src={instance.favicon} alt={`${instance.name}'s icon`} />
-			<span class="px-2 text-sm font-semibold">{instance.name}</span>
+			<span class="pl-2 text-sm font-semibold">{instance.name}</span>
 		</Link>
-		<button onclick={() => removeItem(instance.url)} class="p-2">
-			<Icon icon="ic:baseline-close" />
-		</button>
+		<div class="flex">
+			<Link href={instance.url} class="-scale-x-100 p-2" external>
+				<Icon icon="gridicons:external" />
+			</Link>
+			<button onclick={confirmRemove} class="cursor-pointer p-2">
+				<Icon icon="ic:baseline-close" />
+			</button>
+		</div>
 	</div>
 	<ResponseTimeline {responseTimeInMs} />
 	{#if instance.lastPingAt}
